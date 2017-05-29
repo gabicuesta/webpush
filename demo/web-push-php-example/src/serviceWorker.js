439ae6b269142a4890c501254ba385c426d833f0
gabriel.cuesta.contractor <gabriel.cuesta.contractor@bbva.com>
2017-05-29 12:19:03 +0200

    Adding images to notification

--- demo/web-push-php-example/src/serviceWorker.js
+++ demo/web-push-php-example/src/serviceWorker.js
@@ -1,11 +1,25 @@
 self.addEventListener('push', function (event) {
+
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = 'Push Codelab';
+  const options = {
+    body: 'Yay it works.',
+    icon: 'images/icon.png',
+    badge: 'images/badge.jpg'
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+
+  /*
     if (!(self.Notification && self.Notification.permission === 'granted')) {
         return;
     }
 
     const sendNotification = body => {
         // you could refresh a notification badge here with postMessage API
-        const title = "Título de la notificación";
+        const title = "Web Push example";
 
         return self.registration.showNotification(title, {
             body,
@@ -16,4 +30,5 @@ self.addEventListener('push', function (event) {
         const message = event.data.text();
         event.waitUntil(sendNotification(message));
     }
+    */
 });
