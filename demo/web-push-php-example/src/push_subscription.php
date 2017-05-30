024aaa35b75a7f4fa7473bb38fd5b0e9a692fc95
gabriel.cuesta.contractor <gabriel.cuesta.contractor@bbva.com>
2017-05-30 17:03:10 +0200

    Added sqlite database and php code to work with it

--- demo/web-push-php-example/src/push_subscription.php
+++ demo/web-push-php-example/src/push_subscription.php
@@ -8,15 +8,24 @@ if (!isset($subscription['endpoint'])) {
 
 $method = $_SERVER['REQUEST_METHOD'];
 
+$dir = 'sqlite:/database.sqlite';
+$dbh  = new PDO($dir) or die("cannot open the database");
+
 switch ($method) {
     case 'POST':
         // create a new subscription entry in your database (endpoint is unique)
+        $query = "INSERT INTO subs (endpoint,subkey,token,dateTime,active) VALUES ('". $subscription['endpoint'] ."','". $subscription['key'] ."','". $subscription['token'] ."','". time() ."','1')";
+        $dbh->query($query);
         break;
     case 'PUT':
         // update the key and token of subscription corresponding to the endpoint
+        $query = "UPDATE subs SET subkey='". $subscription['key'] ."',token='". $subscription['token'] ."' WHERE endpoint='". $subscription['endpoint'] ."'";
+        $dbh->query($query);
         break;
     case 'DELETE':
         // delete the subscription corresponding to the endpoint
+        $query = "DELETE FROM subs WHERE endpoint='". $subscription['endpoint'] ."'";
+        $dbh->query($query);
         break;
     default:
         echo "Error: method not handled";
