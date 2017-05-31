67b38a5bf3e1805a7d8b6fa2c6098c5bffdf98b9
gabriel.cuesta.contractor <gabriel.cuesta.contractor@bbva.com>
2017-05-31 22:27:19 +0200

    Modified sqlite code

--- demo/web-push-php-example/src/push_subscription.php
+++ demo/web-push-php-example/src/push_subscription.php
@@ -6,21 +6,28 @@ if (!isset($subscription['endpoint'])) {
     return;
 }
 
-$method = $_SERVER['REQUEST_METHOD'];
+class MiBD extends SQLite3
+{
+    function __construct()
+    {
+        $this->open('database.sqlite');
+    }
+}
 
-$dir = 'sqlite:/database.sqlite';
-$dbh  = new PDO($dir) or die("cannot open the database");
+$bd = new MiBD();
+
+$method = $_SERVER['REQUEST_METHOD'];
 
 switch ($method) {
     case 'POST':
         // create a new subscription entry in your database (endpoint is unique)
         $query = "INSERT INTO subs (endpoint,subkey,token,dateTime,active) VALUES ('". $subscription['endpoint'] ."','". $subscription['key'] ."','". $subscription['token'] ."','". time() ."','1')";
-        $dbh->query($query);
+        $bd->exec($query);
         break;
     case 'PUT':
         // update the key and token of subscription corresponding to the endpoint
-        $query = "UPDATE subs SET subkey='". $subscription['key'] ."',token='". $subscription['token'] ."' WHERE endpoint='". $subscription['endpoint'] ."'";
-        $dbh->query($query);
+        $query = "INSERT INTO subs (endpoint,subkey,token,dateTime,active) VALUES ('". $subscription['endpoint'] ."','". $subscription['key'] ."','". $subscription['token'] ."','". time() ."','1')";
+        $bd->exec($query);
         break;
     case 'DELETE':
         // delete the subscription corresponding to the endpoint
