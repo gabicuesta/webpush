3015a3a99ba70e7d5584016db213967d583519d1
gabriel.cuesta.contractor <gabriel.cuesta.contractor@bbva.com>
2017-06-01 12:42:11 +0200

    Fix to PUT method

--- demo/web-push-php-example/src/push_subscription.php
+++ demo/web-push-php-example/src/push_subscription.php
@@ -21,36 +21,18 @@ $method = $_SERVER['REQUEST_METHOD'];
 switch ($method) {
     case 'POST':
         // create a new subscription entry in your database (endpoint is unique)
-        $query = "SELECT * FROM subs WHERE enpoint='". $subscription['endpoint'] ."'";
-        $ret = $bd->query($query);
-
-        $contador = 0;
-        while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
-            $contador = 1;
-        }
-        if($contador==0){
-          $query = "INSERT INTO subs (endpoint,subkey,token,dateTime,active) VALUES ('". $subscription['endpoint'] ."','". $subscription['key'] ."','". $subscription['token'] ."','". time() ."','1')";
-          $bd->exec($query);
-        }
+        $query = "INSERT INTO subs (endpoint,subkey,token,dateTime,active) VALUES ('". $subscription['endpoint'] ."','". $subscription['key'] ."','". $subscription['token'] ."','". time() ."','1')";
+        $bd->exec($query);
         break;
     case 'PUT':
         // update the key and token of subscription corresponding to the endpoint
-        $query = "SELECT * FROM subs WHERE enpoint='". $subscription['endpoint'] ."'";
-        $ret = $bd->query($query);
-        
-        $contador = 0;
-        while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
-            $contador = 1;
-        }
-        if($contador==0){
-          $query = "INSERT INTO subs (endpoint,subkey,token,dateTime,active) VALUES ('". $subscription['endpoint'] ."','". $subscription['key'] ."','". $subscription['token'] ."','". time() ."','1')";
-          $bd->exec($query);
-        }
+        $query = "INSERT INTO subs (endpoint,subkey,token,dateTime,active) VALUES ('". $subscription['endpoint'] ."','". $subscription['key'] ."','". $subscription['token'] ."','". time() ."','1')";
+        $bd->exec($query);
         break;
     case 'DELETE':
         // delete the subscription corresponding to the endpoint
         $query = "DELETE FROM subs WHERE endpoint='". $subscription['endpoint'] ."'";
-        $dbh->query($query);
+        $bd->exec($query);
         break;
     default:
         echo "Error: method not handled";
