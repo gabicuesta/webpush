c07736fa4482996ed82dac95b0f94cd2ed7119f0
gabriel.cuesta.contractor <gabriel.cuesta.contractor@bbva.com>
2017-06-19 23:21:53 +0200

    changing files from folder

---
+++ panel/send_push_notification.php
@@ -0,0 +1,48 @@
+<?php
+require __DIR__ . '/../demo/vendor/autoload.php';
+use Minishlink\WebPush\WebPush;
+
+require("config.inc.php");
+
+$auth = array(
+    'VAPID' => array(
+        'subject' => 'https://github.com/Minishlink/web-push-php-example/',
+        'publicKey' => 'BCmti7ScwxxVAlB7WAyxoOXtV7J8vVCXwEDIFXjKvD-ma-yJx_eHJLdADyyzzTKRGb395bSAtxlh4wuDycO3Ih4',
+        'privateKey' => 'HJweeF64L35gw5YLECa-K7hwp3LLfcKtpdRNK8C_fPQ', // in the real world, this would be in a secret file
+    ),
+);
+
+$webPush = new WebPush($auth);
+
+$query = "SELECT * FROM notifications ORDER BY id_notifications DESC LIMIT 0,1";
+$ret = $conn->query($query);
+
+$row = mysqli_fetch_array($ret,MYSQLI_NUM);
+
+$title    = $row[2];
+$content  = $row[3];
+$icon     = $row[4];
+$image    = $row[5];
+
+$query = "SELECT * FROM subs";
+$ret = $conn->query($query);
+
+if ($ret->num_rows > 0) {
+    // output data of each row
+    while($row = $ret->fetch_assoc()) {
+	$endpoint = $row['endpoint'];
+    	$key      = $row['subkey'];
+    	$token    = $row['token'];
+
+    	$res = $webPush->sendNotification(
+        	$endpoint,
+        	"{\"title\":\"". $title ."\",\"content\":\"". $content ."\",\"icon\":\"". $icon ."\",\"image\":\"". $image ."\"}",
+        	$key,
+        	$token,
+        	true
+    	);
+    }
+} else {
+    echo "0 results";
+}
+?>
