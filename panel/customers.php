44817bf7729988ee1f2713a6006f1a25c4f6e1df
gabriel.cuesta.contractor <gabriel.cuesta.contractor@bbva.com>
2017-06-20 09:25:56 +0200

    Customers added

--- panel/customers.php
+++ panel/customers.php
@@ -7,3 +7,22 @@
  */
 
 require("config.inc.php");
+
+// Formulario de inserción
+
+
+// Listado
+$query = "SELECT * FROM customers ORDER BY id_customers DESC";
+$ret = $conn->query($query);
+
+echo("<ul>");
+
+if ($ret->num_rows > 0) {
+    while($row = $ret->fetch_assoc()) {
+        echo("<li>");
+        echo($row['name']);
+        echo("<li>");
+    }
+}
+
+echo("</ul>");
